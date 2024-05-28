713bb3168bab59f9720de8a057ab9bda1908aa03
swanil-sharma7222 <2021uee1371@mnit.ac.in>
2024-05-28 12:04:30 +0530

    Added sample server

--- server/server.js
+++ server/server.js
@@ -1,15 +1,25 @@
 const express = require('express')
 const connectdb = require('./db')
-const app=express();
-const bodyParser = require('body-parser')
+const bodyParser = require('body-parser');
+const {WebSocketServer}= require('ws');
+
+const app = express();
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }))
-require('dotenv').config()
+require('dotenv').config();
 connectdb();
-app.use('/',require('./routes/taskRoutes'));
-app.use('/user',require('./routes/userRoutes'));
 
-app.listen(5000, () => {
-    console.log('serving on port 5000....');
+app.use('/', require('./routes/taskRoutes'));
+app.use('/user', require('./routes/userRoutes'));
+
+const server= app.listen(5000, () => {
+  console.log('serving on port 5000....');
+});
+
+const wss= new WebSocketServer({server});
+wss.on('connection', (ws)=> {
+  ws.on('message', (data)=> {
+    console.log('this is data from client %s', data);
+    ws.send('thanks buddy!!');
   })
-  
+})
